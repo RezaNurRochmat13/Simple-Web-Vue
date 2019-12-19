6e90a43bb45d898ba079fd9e7d14302d35081c49
rezanurrochmat@gmail.com <rezanurrochmat@gmail.com>
2019-12-19 21:31:29 +0700

    Initial commits

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/meitaku.jpg" style="width: 300px; height: 400px; margin-right: 50px;">
+    <img alt="Vue logo" src="./assets/meitaku2.jpg" style="width: 300px; height: 400px; margin-right: 50px;">
+    <img alt="Vue logo" src="./assets/meitaku3.jpg" style="width: 300px; height: 400px;">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,20 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container" style="margin-top: 50px;">
+    <div class="card">
+      <div class="card-header">Kartu puisi cinta dariku :*</div>
+      <div class="card-body">
+        <p>Meitaku...</p>
+        <p>Setiap menit setiap detik, diriku selalu kangen dengan kehadiranmu disini :)</p>
+        <p>Senyuman itu adalah bukti akan betapa tulusnya cintamu kepadaku</p>
+        <p>Setiap sapaan sayangmu itu menggetarkan kalbuku yang udah lama mati :)</p>
+        <p>Makasih ya sayang, kamu udah mewarnai hidupku :)</p>
+        <p>Sayang mau kan jadi calon istriku?</p>
+        <p>Semoga kamu mau ya :)</p>
+        <p>Kalo sayang mau, besok akhir tahun aku lamar kamu sayang :*</p>
+        <p>Yang tersayang calon suamimu</p>
+        <p>Semoga kamu suka puisinya ya :*</p>
+      </div>
+    </div>
   </div>
 </template>
 
